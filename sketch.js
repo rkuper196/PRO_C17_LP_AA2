67215a0a404b8e5194493ecb49d83c61efa4496e
gabrielqueiroz08 <100386457+gabrielqueiroz08@users.noreply.github.com>
2022-05-14 10:38:43 -0300

    primeiroCommit

--- sketch.js
+++ sketch.js
@@ -1,64 +1,62 @@
 var PLAY = 1;
 var END = 0;
 var gameState = PLAY;
-
+var mario1,mario2,mario3,mario4,mario5,mario6,mario7,mario8,mario9,mario10,mario_running
 var trex, trex_running, trex_collided;
 var ground, invisibleGround, groundImage;
-
-var cloudsGroup, cloudImage;
+var goombarunning
+var cloud, cloudsGroup, cloudImage;
 var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
-
+var goomba
 var score;
 var gameOverImg,restartImg
 var jumpSound , checkPointSound, dieSound
 
-function preload(){
-  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+function preload() {
+  trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
   trex_collided = loadAnimation("trex_collided.png");
-  
+  jumpSound = loadSound("jump.mp3")
   groundImage = loadImage("ground2.png");
-  
+  dieSound = loadSound("die.mp3")
+  checkPointSound = loadSound("checkpoint.mp3")
   cloudImage = loadImage("cloud.png");
-  
+  mario_running = loadAnimation("mario_1.png","mario_2.png","mario_3.png")
+  goombarunning = loadAnimation("goomba1.png","goomba2.png")
   obstacle1 = loadImage("obstacle1.png");
   obstacle2 = loadImage("obstacle2.png");
   obstacle3 = loadImage("obstacle3.png");
   obstacle4 = loadImage("obstacle4.png");
   obstacle5 = loadImage("obstacle5.png");
   obstacle6 = loadImage("obstacle6.png");
-  
-  restartImg = loadImage("restart.png")
-  gameOverImg = loadImage("gameOver.png")
-  
-  jumpSound = loadSound("jump.mp3")
-  dieSound = loadSound("die.mp3")
-  checkPointSound = loadSound("checkPoint.mp3")
+  gameOver = loadImage("gameOver.png")
+  restart = loadImage("restart.png")
 }
 
 function setup() {
   createCanvas(600, 200);
 
   var message = "Esta é uma mensagem";
- console.log(message)
   
   trex = createSprite(50,160,20,50);
-  trex.addAnimation("running", trex_running);
+  trex.addAnimation("running", mario_running);
   trex.addAnimation("collided", trex_collided);
-  
+  goomba = createSprite(200,180,20,50);
+  goomba.addAnimation("goombarunning",goombarunning)
+  goomba.scale = 0.3
 
   trex.scale = 0.5;
-  
   ground = createSprite(200,180,400,20);
   ground.addImage("ground",groundImage);
   ground.x = ground.width /2;
   
+  console.log(message)
   gameOver = createSprite(300,100);
   gameOver.addImage(gameOverImg);
   
   restart = createSprite(300,140);
   restart.addImage(restartImg);
   
- 
+  
   gameOver.scale = 0.5;
   restart.scale = 0.5;
   
@@ -70,7 +68,7 @@ function setup() {
   cloudsGroup = createGroup();
 
   
-  trex.setCollider("rectangle",0,0,trex.width,trex.height);
+  trex.setCollider("circle",0,0,40);
   trex.debug = true
   
   score = 0;
@@ -79,15 +77,15 @@ function setup() {
 
 function draw() {
   
-  background(180);
+  background("white");
   //exibir pontuação
   text("Pontuação: "+ score, 500,50);
   
   
   if(gameState === PLAY){
 
-    gameOver.visible = false;
-    restart.visible = false;
+   gameOver.visible = false;
+   restart.visible = false;
     
     ground.velocityX = -(4 + 3* score/100)
     //pontuação
@@ -102,7 +100,7 @@ function draw() {
     }
     
     //pular quando barra de espaço é pressionada
-    if(keyDown("space")&& trex.y >= 100) {
+    if(keyDown("space")&& trex.y >= 160) {
         trex.velocityY = -12;
         jumpSound.play();
     }
@@ -127,11 +125,13 @@ function draw() {
    else if (gameState === END) {
       gameOver.visible = true;
       restart.visible = true;
-     
+      
      //mudar a animação de trex
       trex.changeAnimation("collided", trex_collided);
     
-     
+      if(mousePressedOver(restart)) {
+        reset();
+      }
      
       ground.velocityX = 0;
       trex.velocityY = 0
@@ -149,50 +149,54 @@ function draw() {
   //impedir que trex caia
   trex.collide(invisibleGround);
   
-  if(mousePressedOver(restart)) {
-      reset();
-    }
+
 
 
   drawSprites();
 }
 
 function reset(){
-  
-
+  gameState = PLAY
+  obstaclesGroup.destroyEach();
+  cloudsGroup.destroyEach();
+  trex.changeAnimation("running",trex_running)
+  score = 0 
 }
 
 
-function spawnObstacles(){
- if (frameCount % 60 === 0){
-   var obstacle = createSprite(600,165,10,40);
-   obstacle.velocityX = -(6 + score/100);
-   
-    //gerar obstáculos aleatórios
-    var rand = Math.round(random(1,6));
-    switch(rand) {
-      case 1: obstacle.addImage(obstacle1);
-              break;
-      case 2: obstacle.addImage(obstacle2);
-              break;
-      case 3: obstacle.addImage(obstacle3);
-              break;
-      case 4: obstacle.addImage(obstacle4);
-              break;
-      case 5: obstacle.addImage(obstacle5);
-              break;
-      case 6: obstacle.addImage(obstacle6);
-              break;
-      default: break;
-    }
-   
-    //atribuir dimensão e tempo de vida ao obstáculo           
+
+function spawnObstacles() {
+  if (frameCount % 60 === 0) {
+    var obstacle = createSprite(400, 165, 10, 40);
+    obstacle.velocityX = -6;
+    obstacle.addAnimation("goombarunning",goombarunning)
+    obstacle.debug = true 
+
+    // //gerar obstáculos aleatórios
+    var rand = Math.round(random(1, 6));
+    // switch (rand) {
+    //   case 1: obstacle.addAnimation(goombarunning);
+    //     break;
+    //   case 2: obstacle.addImage(obstacle2);
+    //     break;
+    //   case 3: obstacle.addImage(obstacle3);
+    //     break;
+    //   case 4: obstacle.addImage(obstacle4);
+    //     break;
+    //   case 5: obstacle.addImage(obstacle5);
+    //     break;
+    //   case 6: obstacle.addImage(obstacle6);
+    //     break;
+    //   default: break;
+    // }
+
+    //atribuir escala e vida útil ao obstáculo          
     obstacle.scale = 0.5;
     obstacle.lifetime = 300;
-   
-   //acrescentar cada obstáculo ao grupo
+
+    //adicione cada obstáculo ao grupo
     obstaclesGroup.add(obstacle);
- }
+  }
 }
 
 function spawnClouds() {
